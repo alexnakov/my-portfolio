b86bbc7c218e11af41bfc1d579cb8071a96625fe
alexnakov <alex.nakov3@gmail.com>
2024-08-02 12:52:07 +0100

    Initial homepage b uild

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import MenuIcon from './components/MenuIcon';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='nav-wrapper'>
+          <div>Homepage</div>
+          <MenuIcon />
+        </div>
+        <div className='intro-wrapper'>
+          <br/>
+          <span>Hi, I'm Alex</span>
+          <br/><br/>
+          <span>a front-end web developer.</span>
+        </div>
+        <div className='prof-img-wrapper'>
+          <img src='portfolio-funny-ppic.jpg' alt='Profile img'/>
+        </div>
       </header>
     </div>
   );
